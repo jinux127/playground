4c70c2f418509af2d81f90bc3db9d1f537b37042
jinwoo <wwo1278@gmail.com>
2022-09-11 00:16:25 +0900

    fix: 폴더 구조 변경

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 // 리액트 정적 파일 제공
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // 라우트 설정
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 console.log(`server running at http ${port}`);
