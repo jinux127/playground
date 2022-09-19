5bb5e7fd72885e576788b585ff5d6eb4e15cca9b
jinwoo <wwo1278@gmail.com>
2022-09-20 00:25:24 +0900

    fix: MemoArticles 테스트데이터 제거

--- client/src/MemoArticles/index.ts
+++ client/src/MemoArticles/index.ts
@@ -7,16 +7,6 @@ const MemoArticles: IMemo[] = [
     date: '2022년 09월 15일',
     content: InfiniteScroll(),
   },
-  {
-    title: '테스트',
-    date: '2022년 09월 15일',
-    content: InfiniteScroll(),
-  },
-  {
-    title: '테스트2',
-    date: '2022년 09월 15일',
-    content: InfiniteScroll(),
-  },
 ];
 
 export default MemoArticles;
