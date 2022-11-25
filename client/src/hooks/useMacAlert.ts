a44fa845e77847ba0471a4a0bf0430afabfb9a68
jinwoo <wwo1278@gmail.com>
2022-11-26 00:23:56 +0900

    fix: 연속 클릭 알람
    1. debounce 커스텀 훅 생성

--- client/src/hooks/useMacAlert.ts
+++ client/src/hooks/useMacAlert.ts
@@ -1,19 +1,18 @@
 import { useEffect, useState } from 'react';
+import useDebounce from './useDebounce';
 
 const useMacAlert = () => {
   const [macAlert, setMacAlert] = useState({ title: '', url: '', icon: '', isView: false });
-
   const handleCloseAlert = () => {
     setMacAlert(cur => ({ ...cur, isView: false }));
   };
 
+  const debounceMacAlert = useDebounce(() => handleCloseAlert(), 3000);
   useEffect(() => {
     if (macAlert.isView) {
-      setTimeout(() => {
-        handleCloseAlert();
-      }, 3000);
+      debounceMacAlert();
     }
-  }, [macAlert]);
+  }, [macAlert, debounceMacAlert]);
 
   return { macAlert, handleCloseAlert, setMacAlert };
 };
